d42fbb8e60ac16d6b345bc45467f85a71dd24052
alexrsing <alex@sing.email>
2024-06-03 11:13:34 -0700

    Refactoring completed. Move and upload log file needed for Onedrive. Dropbox not tested. keepOriginalFile method not working.

---
+++ src/main/java/email/sing/tools/dropbox/deduper/DedupeFileAccessor.java
@@ -0,0 +1,18 @@
+package email.sing.tools.dropbox.deduper;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public interface DedupeFileAccessor {
+
+    void init();
+
+    List<GenericFileMetadata> getFiles(String path, boolean recursive) throws Exception;
+
+    void moveFilesToFolder(Map<String, List<GenericFileMetadata>> files) throws Exception;
+
+    void deleteFiles(Map<String, List<GenericFileMetadata>> map);
+
+    void uploadLogFile(File file);
+}
